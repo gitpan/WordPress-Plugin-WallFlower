f710947e484f510c0051600b638fb5f7adbe921e
C.J. Adams-Collier <cjac@colliertech.org>
2012-08-11 01:11:05 +0000

    Import of CJCOLLIER/WordPress-Plugin-WallFlower-0.0.2 from CPAN.
    gitpan-cpan-distribution: WordPress-Plugin-WallFlower
    gitpan-cpan-version:      0.0.2
    gitpan-cpan-path:         CJCOLLIER/WordPress-Plugin/WordPress-Plugin-WallFlower-0.0.2.tar.gz
    gitpan-cpan-author:       CJCOLLIER
    gitpan-cpan-maturity:     released

---
+++ wallflower.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ Plugin Name: colliertech.org' Wall Flower
+ Plugin URI: http://wp.colliertech.org/downloads/wallflower.zip
+ Description: colliertech.org' Wall Flower mostly holds the wall up at dances..
+ Author: C.J. Adams-Collier
+ Version: 0.0.2
+ Author URI: http://wp.colliertech.org/cj/
+ */
+
+include_once('license.php');
+
+echo "boy.  is this thing ever on.";
+
+?>
+
